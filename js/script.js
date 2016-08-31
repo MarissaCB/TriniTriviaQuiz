996bdea6d15d5321c550fa33be29324f5d817106
MarissaCB <marissacody@gmail.com>
2016-08-31 02:44:50 -0400

    css and html updated

--- js/script.js
+++ js/script.js
@@ -3,31 +3,9 @@
 //prompt("Please enter a number ");
 
 $( document ).ready(function() {
-	$("button").on("click", function(){
-	var userValue = $(".userValue").val();	
-
-for (var counter = userValue; counter <= 100; counter++) {
-
-
-if (counter % 5 == 0 && counter % 3 == 0)  {
-	console.log("FizzBuzz");
-}
-
-
-else if (counter % 3 == 0) {
-	console.log("Fizz");
-}
-
-
-else if (counter % 5 == 0) { 
-	console.log("Buzz");
-}
-
-
-else {
-	console.log(counter);
+	
 }
 
 }
 });
-});
+
