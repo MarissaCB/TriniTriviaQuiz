b341f3d5441d8ea4213f95a9ac5f201911ba15aa
MarissaCB <marissacody@gmail.com>
2016-08-31 20:24:38 -0400

    results page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 
 
-//prompt("Please enter a number ");
-
 $( document ).ready(function() {
-	
-}
 
-}
+
+/*--- To count the number of User guesses ---*/
+	$("#submitButton").on("click", function(e){
+		$("#count").html(function(i, val) { return +val+1 });       
+});
+
+var question = {
+	question: ""
+
+};	
+	
+/*--- Display information modal box ---*/
+  	$(".score").click(function(){
+    	$(".resultPage").fadeIn(1000);
+  	});
+
+ /*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".resultPage").fadeOut(1000);
+  	});
+
 });
 
